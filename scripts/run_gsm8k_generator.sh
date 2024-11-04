a4c072ec805604505c0e4a78d31855ea438b20b9
emsjob <emilsjob@protonmail.com>
2024-11-04 16:15:13 +0100

    adding requirements

--- scripts/run_gsm8k_generator.sh
+++ scripts/run_gsm8k_generator.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python run_src/do_generate.py \
+CUDA_VISIBLE_DEVICES=0 python3 run_src/do_generate.py \
     --dataset_name GSM8K \
     --test_json_filename test_all \
     --model_ckpt mistralai/Mistral-7B-v0.1 \
